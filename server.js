219fc388f646339fa7a1ea10b6f8a36501a7cf1b
Stephen Siciliano <stepsic@microsoft.com>
2015-07-09 21:41:54 -0700

    change the signature to require a return

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
 var vm = require('vm');
-	
 
 var app = express();
 
@@ -13,47 +12,38 @@ var server = http.createServer(app);
 
 var jsonParser = bodyParser.json();
 
-app.post('/script', jsonParser, function(req,res){
+function executionHandler(isTrigger){
+	return function(req,res){
 	if(!req.body || !req.body.code || !req.body.sandbox){
 		console.log('The request failed because it was missing parameters:\n' + JSON.stringify(req.body));
 		return res.status(400).send('You need to provide code to execute and a sandbox that contains any objects you want to run the code over.');
 	}
 	
-	var sandbox = req.body.sandbox;
-	
 	try{
-		vm.runInNewContext(req.body.code, sandbox);
-	}catch(error){
-		console.log('The execution of the JavaScript code:\n' + req.body.code + '\nAnd Context:\n' +  JSON.stringify(req.body.sandbox) + '\nFailed with error:\n' + error);
-		return res.status(400).send('The code you provided has an error in it. You must provide vaid JavaScript code that only references data in the sandbox.\n\n' + error);
-	}
-	
-	res.json(sandbox);
-});
-
-app.post('/trigger', jsonParser, function(req,res){
-	if(!req.body || !req.body.code || !req.body.sandbox){
-		console.log('The request failed because it was missing parameters:\n' + JSON.stringify(req.body));
-		return res.status(400).send('You need to provide code to execute and a sandbox that contains any objects you want to run the code over.');
-	}
-	
-	var sandbox = req.body.sandbox;
-	
-	try{
-		vm.runInNewContext(req.body.code, sandbox);
-		if(sandbox.trigger == null){
-			console.log('The caller did not provide a trigger for:\n' + req.body.code + '\nAnd Context:\n' +  JSON.stringify(req.body.sandbox));
-			return res.status(400).send('To write a script trigger you must return a boolean property called \'trigger\' that is either true or false.');			
-		} else if (sandbox.trigger == false){
-			return res.status(202).send(sandbox);
+		var sandbox = req.body.sandbox;
+		var code = "function code(){\n" +  req.body.code + "\n}\n 	var result = code();";
+		try{
+			vm.runInNewContext(code, sandbox);
+			if (isTrigger && !sandbox.result){
+				return res.status(202).send(sandbox.result);
+			}						
+		}catch(error){
+			console.log('The execution of the JavaScript code:\n' + req.body.code + '\nAnd Context:\n' +  JSON.stringify(req.body.sandbox) + '\nFailed with error:\n' + error);
+			return res.status(400).send('The code you provided has an error in it. You must provide vaid JavaScript code that only references data in the sandbox.\n\n' + error);
 		}
+		res.json(sandbox.result);
+
 	}catch(error){
-		console.log('The execution of the JavaScript code:\n' + req.body.code + '\nAnd Context:\n' +  JSON.stringify(req.body.sandbox) + '\nFailed with error:\n' + error);
-		return res.status(400).send('The code you provided has an error in it. You must provide vaid JavaScript code that only references data in the sandbox.\n\n' + error);
+			console.log('The execution of the JavaScript code:\n' + req.body.code + '\nAnd Context:\n' +  JSON.stringify(req.body.sandbox) + '\nFailed with error:\n' + error);
+			return res.status(400).send('The context must be a string of valid JSON. If you already have a JSON object, you can use the @string() function to convert it to a JSON string.\n\n' + error);
+		
 	}
-	
-	res.json(sandbox);
-});
+};
+}
+
+app.post('/script', jsonParser,  executionHandler(false));
+
+app.post('/trigger', jsonParser, executionHandler(true));
 
 app.get('/swagger/docs/v1', function(req,res){
 	var fs = require('fs');
